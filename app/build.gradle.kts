14646a4a265dbdfec5be6e9bdadc681a65de637f
OSilvaDev <yorichiiffx@gmail.com>
2024-10-05 01:35:59 -0400

    Update build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,14 @@ plugins {
 
 android {
   namespace = "com.silva.nexuside"
+  compileSdk = 34
 
   defaultConfig {
     applicationId = "com.silva.nexuside"
+    minSdk = 26
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
 
     vectorDrawables.useSupportLibrary = true
 
