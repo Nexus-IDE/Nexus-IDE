6ed84161ce5aba465fd27fca5637df350f3800b4
OSilvaDev <yorichiiffx@gmail.com>
2024-10-08 20:58:53 -0400

    fix: added layout margin, feat: DynamicColors

--- app/src/main/java/com/silva/nexuside/NexusIDEApplication.java
+++ app/src/main/java/com/silva/nexuside/NexusIDEApplication.java
@@ -7,9 +7,9 @@ public class NexusIDEApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         // Registrar o CrashHandler
         CrashHandler crashHandler = new CrashHandler(this);
         Thread.setDefaultUncaughtExceptionHandler(crashHandler);
+        DynamicColors.applyToActivitiesIfAvailable(this);
     }
 }
