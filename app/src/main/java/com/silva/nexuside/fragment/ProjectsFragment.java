9d73181f1349720e508eebb61ca9f7fb031bc0fb
OSilvaDev <yorichiiffx@gmail.com>
2024-10-11 00:41:46 -0400

    fix: import add

--- app/src/main/java/com/silva/nexuside/fragment/ProjectsFragment.java
+++ app/src/main/java/com/silva/nexuside/fragment/ProjectsFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
+import com.google.android.material.transition.MaterialSharedAxis;
 import com.silva.nexuside.databinding.FragmentProjectsBinding;
 
 public class ProjectsFragment extends Fragment {
