133b8320f6e6ebd6e7a44ce38d89a4f36cae5348
trindadedev13 <devsuay@gmail.com>
2024-10-16 01:15:21 -0300

    fix: crash when try open settings because are not inflated

--- app/src/main/java/com/silva/nexuside/ui/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/silva/nexuside/ui/fragments/settings/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.silva.nexuside.ui.fragments.settings
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 import androidx.fragment.app.Fragment
 
-import com.silva.nexuside.resources.R
 import com.silva.nexuside.databinding.FragmentSettingsBinding
 import com.silva.nexuside.ui.base.BaseFragment
 
@@ -12,18 +14,26 @@ class SettingsFragment(
     private val startFragment: Fragment
 ) : BaseFragment() {
 
-    private lateinit var binding: FragmentSettingsBinding
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = FragmentSettingsBinding.inflate(layoutInflater)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         openFragment(startFragment, startFragment.toString())
+        return binding.root
     }
-    
+
     private fun openFragment(fragment: Fragment, lue: String) {
         parentFragmentManager.beginTransaction()
             .replace(binding.fragmentSettings.id, fragment, lue)
             .addToBackStack(null)
             .commit()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
