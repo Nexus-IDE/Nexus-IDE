0a8de6276055b8c594072efc51d6601f272e65ff
trindadedev13 <devsuay@gmail.com>
2024-10-16 02:55:57 -0300

    fix: missing ()

--- app/src/main/java/com/silva/nexuside/NexusIDEApp.kt
+++ app/src/main/java/com/silva/nexuside/NexusIDEApp.kt
@@ -3,7 +3,7 @@ package com.silva.nexuside
 import android.app.Application
 import com.google.android.material.color.DynamicColors
 
-class NexusIDEApp: Application {
+class NexusIDEApp: Application() {
 
     override fun onCreate() {
         DynamicColors.applyToActivitiesIfAvailable(this)
