c2e3a80c2005aac7705a635b84b5b40e947d51c4
trindadedev13 <devsuay@gmail.com>
2024-10-15 23:06:47 -0300

    feat: better anims

--- app/src/main/java/com/silva/nexuside/ui/base/BaseFragments.kt
+++ app/src/main/java/com/silva/nexuside/ui/base/BaseFragments.kt
@@ -2,6 +2,7 @@ package com.silva.nexuside.ui.base
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
@@ -9,22 +10,39 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.color.MaterialColors
 import com.google.android.material.transition.MaterialSharedAxis
 
+val sharedEnter = MaterialSharedAxis(MaterialSharedAxis.X, true).apply {
+    duration = 700
+    interpolator = AccelerateDecelerateInterpolator()
+}
+val sharedExit = MaterialSharedAxis(MaterialSharedAxis.X, true).apply {
+    duration = 700
+    interpolator = AccelerateDecelerateInterpolator()
+}
+val sharedReturn = MaterialSharedAxis(MaterialSharedAxis.X, false).apply {
+    duration = 700
+    interpolator = AccelerateDecelerateInterpolator()
+}
+val sharedReenter = MaterialSharedAxis(MaterialSharedAxis.X, false).apply {
+    duration = 700
+    interpolator = AccelerateDecelerateInterpolator()
+}
+
 open class BaseFragment() : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
-        returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
-        exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
-        reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+        enterTransition = sharedEnter
+        returnTransition = sharedReturn
+        exitTransition = sharedExit
+        reenterTransition =  sharedReenter
     }
 }
 
 open class BasePreferenceFragment() : PreferenceFragmentCompat() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
-        returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
-        exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
-        reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+        enterTransition = sharedEnter
+        returnTransition = sharedReturn
+        exitTransition = sharedExit
+        reenterTransition =  sharedReenter
     }
 }
