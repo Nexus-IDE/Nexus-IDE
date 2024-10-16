f73f9162f17ff7a6509e64702c55dc6ef9194e0e
trindadedev13 <devsuay@gmail.com>
2024-10-16 02:43:07 -0300

    refactor: Kotlin in AppClass

---
+++ app/src/main/java/com/silva/nexuside/NexusIDEApp.kt
@@ -0,0 +1,11 @@
+package com.silva.nexuside;
+
+import android.app.Application;
+import com.google.android.material.color.DynamicColors
+
+class NexusIDEApp: Application {
+
+    override fun onCreate() {
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
