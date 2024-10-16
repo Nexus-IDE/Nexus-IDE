1f3d3e9b4188fbeab9d3c8379105120829a8f8db
trindadedev13 <devsuay@gmail.com>
2024-10-15 22:53:06 -0300

    feat: Settings init

---
+++ app/src/main/java/com/silva/nexuside/ui/fragments/settings/SettingsFragment.kt
@@ -0,0 +1,20 @@
+package com.silva.nexuside.ui.fragments.settings
+
+import android.os.Bundle
+
+import androidx.preference.Preference
+import androidx.preference.PreferenceManager
+
+import com.silva.nexuside.R;
+import com.silva.nexuside.ui.base.BasePreferenceFragment
+
+class SettingsFragment : BasePreferenceFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences_main, rootKey)
+    }
+}
