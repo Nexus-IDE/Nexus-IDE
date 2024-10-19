9745711df4a18b723d02cb7575178f496573ae5a
trindadedev13 <devsuay@gmail.com>
2024-10-19 00:07:58 -0300

    feat: re-implement Koin & DataStore

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,4 +19,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "NexusIDE"
 
-include(":app", ":editor", ":util", ":core:resources")
+include(":app", ":editor", ":util", ":core:resources", ":core:settings")
