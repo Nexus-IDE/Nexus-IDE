476001525629f8089a70703dbbf0b3c172a5f953
trindadedev13 <devsuay@gmail.com>
2024-10-18 23:38:34 -0300

    Several Changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
      alias(libs.plugins.android.library) apply false
      alias(libs.plugins.kotlin.android) apply false
      alias(libs.plugins.kotlin.jvm) apply false
-     alias(libs.materialthemebuilder) apply false
+     alias(libs.plugins.materialthemebuilder) apply false
 }
 
 tasks.register<Delete>("clean") {
--- core/resources/build.gradle.kts
+++ core/resources/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   alias(libs.plugins.android.library)
   alias(libs.plugins.kotlin.android)
-  alias(libs.materialthemebuilder)
+  alias(libs.plugins.materialthemebuilder)
 }
 
 android {
